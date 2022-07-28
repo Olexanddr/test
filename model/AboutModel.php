c91427b49a39abcf02fad8d189f832b227b56d5d
Olexanddr <48255226+Olexanddr@users.noreply.github.com>
2022-07-29 02:36:33 +0300

    Update AboutModel.php

--- model/AboutModel.php
+++ model/AboutModel.php
@@ -3,7 +3,7 @@
 		public function getInfo($id)
         {
             $data = array();
-            $query = $this->db->prepare("SELECT * FROM test.conferences WHERE id = :id");
+            $query = $this->db->prepare("SELECT * FROM heroku_dc0e8d8db09d6e3.conferences WHERE id = :id");
             $query->execute(['id' => $id]);
             $result = $query->fetchAll(PDO::FETCH_ASSOC);
             $result[0]["data"] = str_replace('T', ' ', $result[0]["data"]);
