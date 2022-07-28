64f0bd8bd0035c213b25c39051e45b49c51a3c33
Olexanddr <48255226+Olexanddr@users.noreply.github.com>
2022-07-29 02:38:37 +0300

    Update EditModel.php

--- model/EditModel.php
+++ model/EditModel.php
@@ -8,7 +8,7 @@
 			return $result[0];
 		}
 		public function setInfo($info){
-			$query = $this->db->prepare("UPDATE test.conferences SET title = :title, data = :data, latitude = :latitude, longitude = :longitude, country = :country WHERE id = :id");
+			$query = $this->db->prepare("UPDATE heroku_dc0e8d8db09d6e3.conferences SET title = :title, data = :data, latitude = :latitude, longitude = :longitude, country = :country WHERE id = :id");
 			$query->execute($info);
 			$result = $query->fetchAll(PDO::FETCH_ASSOC);
 			header("Location: /conferences");
